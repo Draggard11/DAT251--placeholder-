24dd9ba38f05bbce975e09db59db8b509bb48e9b
Draggard11 <draggard@duck.com>
2026-02-26 22:36:15 +0100

    added simple spring web and fixed backend folder

---
+++ backend/src/test/kotlin/no/hvl/dat251/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.hvl.dat251.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
