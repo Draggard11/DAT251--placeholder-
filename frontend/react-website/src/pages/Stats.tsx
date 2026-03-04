b65aa27ba0a36f1bb94308a6437b5d7b3021b92a
600881 <600881@stud.hvl.no>
2026-03-04 13:39:03 +0100

    Progress bar

--- frontend/react-website/src/pages/Stats.tsx
+++ frontend/react-website/src/pages/Stats.tsx
@@ -1,9 +1,31 @@
 import React from "react";
 
 const Stats = () => {
+    const progress = 40;
     return (
         <div>
-            <h1>this are the stats</h1>
+            <h1>These are your stats:</h1>
+            <div style={{
+                    width: "300px",
+                    height: "20px",
+                    backgroundColor: "#ddd",
+                    borderRadius: "10px",
+                    overflow: "hidden"
+                  }}>
+                    <div style={{
+                      width: progress + "%",
+                      height: "100%",
+                      backgroundColor: "green",
+                      borderRadius: "10px",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      color: "white",
+                      fontWeight: "bold"
+                    }} >
+                  {progress}%
+                  </div>
+            </div>
         </div>
     );
 };
